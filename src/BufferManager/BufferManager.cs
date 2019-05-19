8696fa526dad4b0616e4f991ddc4ef5a7cf276c5
Haoran SUN <3170105365@zju.edu.cn>
2019-05-19 09:15:00 +0800

    ready to re construct

--- src/BufferManager/BufferManager.cs
+++ src/BufferManager/BufferManager.cs
@@ -18,7 +18,7 @@ namespace HYBase.BufferManager
         public bool dirty;      // TRUE if page is dirty
         public int pinCount;    // pin count
         public int pageNum;     // page number for this page
-        public FileStream fd;          // OS file descriptor of this page
+        public FileStream file;          // OS file descriptor of this page
     }
     class BufferManager
     {
@@ -30,20 +30,24 @@ namespace HYBase.BufferManager
         int numPages;
         int pageSize;
         IDictionary<(FileStream, int), int> hashTable;
+        void InitBuffer()
+        {
+            bufferTable =
+                           Range(0, numPages - 1).Select(index => new BufferPageDesc
+                           {
+                               data = new Memory<byte>(new byte[pageSize]),
+                               prev = index - 1,
+                               next = index + 1
+                           }
+                           ).ToArray();
+            bufferTable[0].prev = bufferTable[numPages - 1].next = -1;
+        }
         public BufferManager(int _numPages)
         {
             pageSize = PAGE_SIZE + Marshal.SizeOf<PageHeader>();
             numPages = _numPages;
             hashTable = new Dictionary<(FileStream, int), int>();
-            bufferTable =
-                Range(0, numPages - 1).Select(index => new BufferPageDesc
-                {
-                    data = new Memory<byte>(new byte[pageSize]),
-                    prev = index - 1,
-                    next = index + 1
-                }
-                ).ToArray();
-            bufferTable[0].prev = bufferTable[numPages - 1].next = -1;
+            InitBuffer();
             first = last = -1;
             free = 0;
 
@@ -68,7 +72,7 @@ namespace HYBase.BufferManager
                             let _3 = InsertFree(slot)
                             select bufferTable[slot].data).First();
 
-        // Allocate a new page in the buffer, point *ppBuffer to its location
+
         Either<ErrorCode, Memory<byte>> AllocatePage(FileStream file, int pageNum)
             => hashTable.TryGetValue((file, pageNum)).BiBind<Either<ErrorCode, Memory<byte>>>(
                     Some: _ => Left<ErrorCode, Memory<byte>>(ErrorCode.PAGEINBUF),
@@ -89,37 +93,138 @@ namespace HYBase.BufferManager
                     return
                         from x in Unlink(slot)
                         from y in LinkHead(slot)
-                        select Unit.Default;
+                        select unit;
                 },
                 None: () => Left<ErrorCode, Unit>(ErrorCode.PAGENOTINBUF)
             ).First();
 
 
-        void UnpinPage(FileStream file, int pageNum)
+        Either<ErrorCode, Unit> UnpinPage(FileStream file, int pageNum)
             => hashTable.TryGetValue((file, pageNum)).BiBind<Either<ErrorCode, Unit>>(
                 Some: slot =>
                     ((--bufferTable[slot].pinCount) == 0)
                     ? (from x in Unlink(slot)
                        from y in LinkHead(slot)
-                       select Unit.Default)
-                    : Right<ErrorCode, Unit>(Unit.Default)
+                       select unit)
+                    : Right<ErrorCode, Unit>(unit)
                 ,
                 None: () => Left<ErrorCode, Unit>(ErrorCode.PAGENOTINBUF)
             ).First();
 
-        void FlushPages(FileStream file);                   // Flush pages for file
+        Either<ErrorCode, Unit> FlushPages(FileStream file)
+        {
+            int slot = first;
+            while (slot != -1)
+            {
+                int next = bufferTable[slot].next;
+                if (bufferTable[slot].file == file)
+                {
+                    if (bufferTable[slot].pinCount == 0)
+                    {
+                        var rc = WritePage(file, bufferTable[slot].pageNum, bufferTable[slot].data.Span);
+                        if (rc.IsLeft) return rc;
+                        bufferTable[slot].dirty = false;
+                        if (hashTable.Remove((file, bufferTable[slot].pageNum)))
+                        {
+                            rc = from x in Unlink(slot)
+                                 from y in InsertFree(slot)
+                                 select x;
+                            if (rc.IsLeft) return rc;
+                        }
+                    }
+                }
+                slot = next;
+            }
+            return unit;
+        }                 // Flush pages for file
 
         // Force a page to the disk, but do not remove from the buffer pool
-        void ForcePages(FileStream file, int pageNum);
+        Either<ErrorCode, Unit> ForcePages(FileStream file, int pageNum)
+        {
+            int slot = first;
+            while (slot != -1)
+            {
+                int next = bufferTable[slot].next;
+                if (bufferTable[slot].file == file && bufferTable[slot].pageNum == pageNum)
+                {
+                    if (bufferTable[slot].dirty)
+                    {
+                        var rc = WritePage(file, bufferTable[slot].pageNum, bufferTable[slot].data.Span);
+                        if (rc.IsLeft) return rc;
+                        bufferTable[slot].dirty = false;
+                    }
+                }
+                slot = next;
+            }
+            return unit;
+        }
 
 
         // Remove all entries from the Buffer Manager.
-        void ClearBuffer();
-        // Display all entries in the buffer
-        void PrintBuffer();
+        Either<ErrorCode, Unit> ClearBuffer()
+        {
+            int slot = first;
+            while (slot != -1)
+            {
+                int next = bufferTable[slot].next;
+
+                if (bufferTable[slot].pinCount == 0)
+                {
+
+                    bufferTable[slot].dirty = false;
+                    if (hashTable.Remove((bufferTable[slot].file, bufferTable[slot].pageNum)))
+                    {
+                        var rc = from x in Unlink(slot)
+                                 from y in InsertFree(slot)
+                                 select x;
+                        if (rc.IsLeft) return rc;
+                    }
+                }
+
+                slot = next;
+            }
+            return unit;
+        }
+
 
         // Attempts to resize the buffer to the new size
-        void ResizeBuffer(int newSize);
+        Either<ErrorCode, Unit> ResizeBuffer(int newSize)
+        {
+            ClearBuffer();
+            var oldBuffer = bufferTable;
+            InitBuffer();
+            int oldFirst = first;
+            numPages = newSize;
+            first = last = -1;
+            free = 0;
+            int slot = oldFirst, next;
+            while (slot != -1)
+            {
+                next = oldBuffer[slot].next;
+                hashTable.Remove((oldBuffer[slot].file, oldBuffer[slot].pageNum));
+                slot = next;
+            }
+            slot = oldFirst;
+            while (slot != -1)
+            {
+                next = oldBuffer[slot].next;
+                var rc =
+                    InternalAlloc().Bind<Unit>(newSlot =>
+                    {
+                        if (hashTable.TryAdd((oldBuffer[slot].file, oldBuffer[slot].pageNum), newSlot))
+                            return Left<ErrorCode, Unit>(ErrorCode.INVALIDPAGE);
+                        var rc1 = InitPageDesc(oldBuffer[slot].file, oldBuffer[slot].pageNum, newSlot);
+                        if (rc1.IsLeft) return rc1;
+                        Unlink(newSlot);
+                        InsertFree(newSlot);
+                        return Right<ErrorCode, Unit>(unit);
+
+                    });
+                if (rc.IsLeft) return rc;
+                slot = next;
+            }
+            return unit;
+        }
 
         // Three Methods for manipulating raw memory buffers.  These memory
         // locations are handled by the buffer manager, but are not
@@ -127,10 +232,17 @@ namespace HYBase.BufferManager
         // want memory that is bounded by the size of the buffer pool.
 
         // Return the size of the block that can be allocated.
-        int GetBlockSize();
+        Either<ErrorCode, int> GetBlockSize()
+        {
+            return pageSize;
+        }
 
         // Allocate a memory chunk that lives in buffer manager
-        Memory<byte> AllocateBlock();
+        Memory<byte> AllocateBlock()
+        {
+            int slot;
+
+        }
         // Dispose of a memory chunk managed by the buffer manager.
         void DisposeBlock(Memory<byte> buffer);
 
@@ -144,7 +256,7 @@ namespace HYBase.BufferManager
         Either<ErrorCode, Memory<byte>> ReadPage(FileStream file, int pageNum);
 
         // Write a page
-        void WritePage(FileStream file, int pageNum, Span<byte> source);
+        Either<ErrorCode, Unit> WritePage(FileStream file, int pageNum, Span<byte> source);
 
         // Init the page desc entry
         Either<ErrorCode, Unit> InitPageDesc(FileStream file, int pageNum, int slot);
