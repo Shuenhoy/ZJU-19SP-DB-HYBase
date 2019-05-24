67a774472757a2a2fb8b494712db984068dea86c
Haoran SUN <3170105365@zju.edu.cn>
2019-05-24 10:22:28 +0800

    add system aka API

---
+++ HYBase/src/System/System.cs
@@ -0,0 +1,14 @@
+using System;
+using HYBase.CatalogManager;
+
+namespace HYBase.System
+{
+    public class DataBaseSystem
+    {
+        void CreateTable(String tableName, AttributeInfo[] attribute)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 1. 安装 .net core
 2. 在本目录 `dotnet restore` 安装依赖库
 3. `dotnet test` 执行单元测试
+
