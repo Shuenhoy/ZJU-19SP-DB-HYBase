4d0837a0da736569731478bed6343e054adf4f1e
Haoran SUN <3170105365@zju.edu.cn>
2019-06-04 21:15:48 +0800

    add system init

--- HYBase/src/System/System.cs
+++ HYBase/src/System/System.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using HYBase.CatalogManager;
 using HYBase.RecordManager;
 using HYBase.IndexManager;
@@ -12,7 +13,15 @@ namespace HYBase.System
         internal RecordManager.RecordManager recoardManager;
         internal IndexManager.IndexManager indexManager;
         internal Interpreter.Interpreter interpreter;
-
+        private BufferManager.PagedFileManager pagedFileManager;
+        public DataBaseSystem()
+        {
+            pagedFileManager = new BufferManager.PagedFileManager();
+            recoardManager = new RecordManager.RecordManager(pagedFileManager);
+            indexManager = new IndexManager.IndexManager(pagedFileManager);
+            catalogManager = new CatalogManager.CatalogManager(recoardManager, new FileStream("rel", FileMode.OpenOrCreate, FileAccess.ReadWrite),
+            new FileStream("attr", FileMode.OpenOrCreate, FileAccess.ReadWrite), new FileStream("index", FileMode.OpenOrCreate, FileAccess.ReadWrite));
+        }
 
     }
 }
