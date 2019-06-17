ac6ae911441bd763353dea8b7070042bd25d41cc
Haoran SUN <shuenhoy@zju.edu.cn>
2019-06-17 09:32:39 +0800

    add sql

---
+++ createTable.sql
@@ -0,0 +1,2 @@
+create table student(sno char(10), sage int, score float, primary key(sno));
+create table book(title char(10), price float, primary key(title));
--- test.sql
+++
@@ -1,2 +0,0 @@
-create table student(sno char(10), sage int, primary key(sno));
-insert into student values('abc',123);
